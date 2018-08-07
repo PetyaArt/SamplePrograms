f90dde39ff2fa8a0c04b7e8873de687e67add18e
PetyaArt <artamonovpetya@gmail.com>
2018-08-07 15:40:38 +0900

    GridView
    GridView

---
+++ StartAndroid/Lesson 57 GridView/app/src/main/java/com/example/petya/lesson57gridview/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.petya.lesson57gridview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridLayout;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] data = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"};
+
+    GridView mGridView;
+    ArrayAdapter<String> mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAdapter = new ArrayAdapter<>(this, R.layout.item, R.id.textView, data);
+
+        mGridView = findViewById(R.id.gridView);
+        mGridView.setAdapter(mAdapter);
+        adjustGridView();
+    }
+
+    private void adjustGridView() {
+        mGridView.setNumColumns(mGridView.AUTO_FIT);
+        mGridView.setColumnWidth(80);
+        mGridView.setVerticalSpacing(5);
+        mGridView.setHorizontalSpacing(5);
+        mGridView.setStretchMode(GridView.STRETCH_SPACING_UNIFORM );
+    }
+}

1db7cf48ab5c5a007ad92ac9a0ff7e5a42e38f7e
PetyaArt <artamonovpetya@gmail.com>
2018-08-16 19:51:01 +0900

    ActionMode
    ActionMode

---
+++ StartAndroid/lesson 113 ActionMode/app/src/main/java/com/example/petya/lesson113actionmode/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.petya.lesson113actionmode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActionMode mActionMode;
+    ListView mListView;
+    final String LOG_TAG = "myLogs";
+
+    String[] data = { "one", "two", "three", "four", "five" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_activated_1, data);
+        mListView = findViewById(R.id.lvActionMode);
+        mListView.setAdapter(arrayAdapter);
+        mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        mListView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+            @Override
+            public void onItemCheckedStateChanged(ActionMode actionMode, int i, long l, boolean b) {
+                Log.d(LOG_TAG, "position = " + i + ", checked = "
+                        + b);
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+                actionMode.getMenuInflater().inflate(R.menu.context, menu);
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+                actionMode.finish();
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode actionMode) {
+
+            }
+        });
+    }
+
+}
